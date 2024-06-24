2460c9de6636ce9a71c5f3722b7ccba45d300dfd
seongmin <sungmin990923@naver.com>
2024-06-24 16:51:03 +0900

    0624 연산자

---
+++ src/Quiz/Quiz1.java
@@ -0,0 +1,18 @@
+package Quiz;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		
+		int num = -10;						// -10
+		int result = +num ;			// -10
+		
+		int num2 = -20;					// -20
+		int result2 = -num2;			// 20
+		
+		System.out.println(result);
+		System.out.println(result2);
+		
+	}
+
+}
